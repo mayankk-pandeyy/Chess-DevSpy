66062a9a52c268b45e50822ac8e5b2d4fbbb0709
Mayank Pandey <immayank7781@gmail.com>
2025-03-05 23:57:32 +0530

    updated

---
+++ backend/src/GameManager.ts
@@ -0,0 +1,55 @@
+import { WebSocket } from "ws";
+import { INIT_GAME, MOVE } from './message';
+import { Game } from "./Game";
+
+
+// }
+
+export class GameManager{
+    private games : Game[];
+    private pendingUser : WebSocket | null;
+    private users : WebSocket[];
+
+    constructor(){
+        this.games = [];
+        this.pendingUser = null;
+        this.users = [];
+    }
+
+    adduser(socket : WebSocket){
+        this.users.push(socket);
+        this.handleMessage(socket);
+    }
+
+    removeUser(socket : WebSocket){
+        this.users = this.users.filter((user)=> user !== socket);
+
+        // Stop the game
+    }
+
+    private handleMessage(socket : WebSocket){
+        socket.on("message", (data)=>{
+            const message = JSON.parse(data.toString());
+
+            if(message.type === INIT_GAME){
+                if(this.pendingUser){
+                    // Start a game
+                    const game = new Game(this.pendingUser, socket);
+
+                    this.games.push(game);
+                    this.pendingUser = null;
+                }else{
+                    this.pendingUser = socket;
+                }
+            }
+
+            if(message.type === MOVE){
+                console.log("Let's make a move");
+                const game = this.games.find((game)=> game.player1 === socket || game.player2 === socket);
+                if(game){
+                    game.makeMove(socket, message.move);
+                }
+            }
+        })
+    }
+}
